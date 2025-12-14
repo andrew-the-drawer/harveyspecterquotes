5f5c2c35aa6e1fbf889a653eff250645b5f162a3
trung <lantrungseo@gmail.com>
2025-12-14 10:44:24 +0700

    Initial Commit

---
+++ harveyspectorquotes/harveyspectorquotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  harveyspectorquotesApp.swift
+//  harveyspectorquotes
+//
+//  Created by Trung Nguyen on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct harveyspectorquotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ harveyspectorquotesTests/harveyspectorquotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  harveyspectorquotesTests.swift
+//  harveyspectorquotesTests
+//
+//  Created by Trung Nguyen on 14/12/25.
+//
+
+import Testing
+@testable import harveyspectorquotes
+
+struct harveyspectorquotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
